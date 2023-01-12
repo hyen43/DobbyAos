823b8645e958a4a85046030b4bcdc2c460901e9d
Nahyun Lee <lnhyen43@gmail.com>
2023-01-12 20:54:14 +0900

    FEAT: home 화면 및 navigation stack 추가

--- android/app/src/main/java/com/dobbyaos/MainActivity.java
+++ android/app/src/main/java/com/dobbyaos/MainActivity.java
@@ -3,6 +3,8 @@ package com.dobbyaos;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle; //react-native navigation
+
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +15,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "DobbyAos";
+  }
+
+//react-native navigation
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
